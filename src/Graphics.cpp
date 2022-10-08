4aed8975ddf0fe6d8921356b96330ee10af45707
Sidharth Das <sidharth2189@gmail.com>
2022-10-08 11:08:20 +0200

    Added traffic simulation

--- src/Graphics.cpp
+++ src/Graphics.cpp
@@ -57,7 +57,7 @@ void Graphics::drawTrafficObjects()
             cv::RNG rng(it->getID());
             int b = rng.uniform(0, 255);
             int g = rng.uniform(0, 255);
-            int r = sqrt(255*255 - g*g - b*b); // ensure that length of color vector is always 255
+            int r = sqrt(255*255 - g*g - r*r); // ensure that length of color vector is always 255
             cv::Scalar vehicleColor = cv::Scalar(b,g,r);
             cv::circle(_images.at(1), cv::Point2d(posx, posy), 50, vehicleColor, -1);
         }
@@ -66,10 +66,8 @@ void Graphics::drawTrafficObjects()
     float opacity = 0.85;
     cv::addWeighted(_images.at(1), opacity, _images.at(0), 1.0 - opacity, 0, _images.at(2));
 
-    // The following code allows for resizing in case of graphics window taking up a large space 
-    cv::Mat img;
-    cv::resize(_images.at(2), img, cv::Size(1040,720),0,0,1);
-    cv::imshow(_windowName, img);
-    
+    // display background and overlay image
+    cv::imshow(_windowName, _images.at(2));
+    cv::resizeWindow(_windowName, 600,600);
     cv::waitKey(33);
 }
--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
 #include <random>
+#include <chrono>
 #include "TrafficLight.h"
 
 /* Implementation of class "MessageQueue" */
 
-/* 
 template <typename T>
 T MessageQueue<T>::receive()
 {
     // FP.5a : The method receive should use std::unique_lock<std::mutex> and _condition.wait() 
     // to wait for and receive new messages and pull them from the queue using move semantics. 
-    // The received object should then be returned by the receive function. 
+    // The received object should then be returned by the receive function.
+
+    // perform message queue modification under lock
+    std::unique_lock<std::mutex> uLock(_mutex);
+
+    // pass unique lock to condition variable
+    _condition.wait(uLock, [this]{return !_queue.empty();});
+
+    // Remove last message from message queue
+    T msg = std::move(_queue.back());
+    _queue.pop_back();
+
+    return msg;
 }
 
 template <typename T>
@@ -18,12 +30,25 @@ void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+
+    // sleep at every iteration to reduce CPU usage
+    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    
+    // perform message queue modification under lock
+    std::lock_guard<std::mutex> uLock(_mutex);
+
+    // prevent backlog of messages
+    _queue.clear();
+
+    // add message to message queue
+    _queue.push_back(std::move(msg));
+    
+    // notify client after pushing message into message queue
+    _condition.notify_one();
 }
-*/
 
 /* Implementation of class "TrafficLight" */
 
-/* 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
@@ -34,6 +59,12 @@ void TrafficLight::waitForGreen()
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
+    
+    while(true) 
+    {
+        // exit loop if traffic light message received from message queue, is green
+        if (_messageQueue.receive() == TrafficLightPhase::green) {return;}
+    }
 }
 
 TrafficLightPhase TrafficLight::getCurrentPhase()
@@ -43,7 +74,9 @@ TrafficLightPhase TrafficLight::getCurrentPhase()
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. 
+    // To do this, use the thread queue in the base class.
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this)); 
 }
 
 // virtual function which is executed in a thread
@@ -53,6 +86,40 @@ void TrafficLight::cycleThroughPhases()
     // and toggles the current phase of the traffic light between red and green and sends an update method 
     // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
     // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
 
-*/
+    // randomized duration of a single simulation cycle in ms (between 4 & 6 seconds)
+    std::random_device rd;
+    std::mt19937 eng(rd());
+    std::uniform_int_distribution<> distr(4000, 6000);
+    double cycleDuration = distr(eng);
+
+    // init stop watch
+    std::chrono::time_point<std::chrono::system_clock> lastUpdate;
+    lastUpdate = std::chrono::system_clock::now();
+
+    // infinite loop to toggle current phase of traffic light
+    while (true)
+    {
+        // sleep at every iteration to reduce CPU usage
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+
+        // compute time difference to stop watch
+        long timeSinceLastUpdate = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - lastUpdate).count();
+        if (timeSinceLastUpdate >= cycleDuration)
+        {            
+            // send update message to queue
+            if (_currentPhase == TrafficLightPhase::red) {
+                // send green
+                _currentPhase = TrafficLightPhase::green;
+            }
+            else if (_currentPhase == TrafficLightPhase::green) {
+                // send red
+                _currentPhase = TrafficLightPhase::red;
+            }
+            _messageQueue.send(std::move(_currentPhase));
+
+            // reset stop watch for next cycle
+            lastUpdate = std::chrono::system_clock::now();
+        }
+    }
+}
